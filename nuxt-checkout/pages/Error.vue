f85a700fc339efac0fd07fe438bb36e9fc70bde9
Leo <lcheung@swellbottle.com>
2021-12-23 15:11:36 -0500

    nuxt-checkout

---
+++ nuxt-checkout/pages/Error.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div class="py-5 text-center">
+      <h2>Error</h2>
+      <p class="lead">Could't process payment!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Error"
+}
+</script>
+
+<style scoped>
+
+</style>
